c43697aaa45ffda21157f53c6f1bd9c0f1ebb5d4
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-24 10:13:47 -0300

    Configuração express e mongoose

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Configuração do App
+const app = express();
+app.use(express.json());
+
+// Configuração do banco de daos
+mongoose.connect(process.env.MONGODB_URL);
+
+// Routes
+
+
+// Escuta
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000/");
+})
